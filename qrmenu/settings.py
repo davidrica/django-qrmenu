d27bc1901ff2c861a36baeaa35f9519b23d9883f
David Ricardo Torres <torresdavidricardo.red@gmail.com>
2023-03-07 12:16:20 -0300

    Settings

--- qrmenu/settings.py
+++ qrmenu/settings.py
@@ -130,14 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#STATIC_ROOT = "/home/webpython/qrmenu/static/"
+STATIC_ROOT = "/home/webpython/qrmenu/static/"
 
 #STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR,'static'),
